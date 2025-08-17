53850befe33e435854f7ccdaf40a36bb2ee8cc36
EmanNasar001 <fatehulnasareman@gmail.com>
2025-08-17 21:15:30 +0500

    Update cachememory.sv

--- 4_SET_ASSOCIATIVE_CACHE/RTL/cachememory.sv
+++ 4_SET_ASSOCIATIVE_CACHE/RTL/cachememory.sv
@@ -25,9 +25,13 @@ module cache_memory #(
     output logic hit,
     output logic [WORD_SIZE-1:0] data_out,
     output logic dirty_bit
-);    typedef struct  {
-        logic b1, b2, b3;
-    } tree_bits;    
+);   
+typedef struct {
+        logic b1;
+        logic b2;
+        logic b3;
+    } tree_bits;
+    
     typedef logic [BLOCK_SIZE + TAG_WIDTH + 2 - 1 : 0] cache_line_t;
     cache_line_t cache [NUM_SETS-1:0][3:0];
     tree_bits plru [NUM_SETS-1:0];    
@@ -58,61 +62,67 @@ module cache_memory #(
         info3.hit   = info3.valid && (tag == info3.tag);
     end    
     assign hit = info0.hit || info1.hit || info2.hit || info3.hit;    
-    function int get_lru_line(tree_bits t);
-        if (t.b1 == 0) begin
-            if (t.b2 == 0) return 0;
-            else           return 1;
+    logic [1:0] lru_line; //LRU replacement
+    always_comb begin
+        if (plru[index].b1 == 0) begin
+            if (plru[index].b2 == 0) lru_line = 0;
+            else                     lru_line = 1;
         end else begin
-            if (t.b3 == 0) return 2;
-            else           return 3;
+            if (plru[index].b3 == 0) lru_line = 2;
+            else                     lru_line = 3;
         end
-    endfunction    
-    task update_tree_on_access(inout tree_bits t, input int line);
-        case (line)
-            0: begin t.b1 = 1; t.b2 = 1; end
-            1: begin t.b1 = 1; t.b2 = 0; end
-            2: begin t.b1 = 0; t.b3 = 1; end
-            3: begin t.b1 = 0; t.b3 = 0; end
+    end   
+    logic [2:0] plru_next;
+    logic [1:0] accessed_line;
+
+    always_comb begin
+        // default hold
+        plru_next = {plru[index].b1, plru[index].b2, plru[index].b3};
+        case (accessed_line)
+            0: begin plru_next[2] = 1; plru_next[1] = 1; end // b1=1, b2=1
+            1: begin plru_next[2] = 1; plru_next[1] = 0; end // b1=1, b2=0
+            2: begin plru_next[2] = 0; plru_next[0] = 1; end // b1=0, b3=1
+            3: begin plru_next[2] = 0; plru_next[0] = 0; end // b1=0, b3=0
         endcase
-    endtask    
+    end
     always_ff @(posedge clk) begin
         data_out <= '0;
         dirty_block_out <= '0;        
         if (!hit) begin //MISS
-            int lru = get_lru_line(plru[index]);            
+                     
             if (!info0.valid && read_en_mem && write_en_cache) begin //when cache is empty
                 cache[index][0][0] <= 1;
                 cache[index][0][1] <= 0;
                 cache[index][0][TAG_WIDTH+1:2] <= tag;
                 cache[index][0][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                update_tree_on_access(plru[index], 0);
+                accessed_line <= 0;
             end else if (!info1.valid && read_en_mem && write_en_cache) begin
                 cache[index][1][0] <= 1;
                 cache[index][1][1] <= 0;
                 cache[index][1][TAG_WIDTH+1:2] <= tag;
                 cache[index][1][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                update_tree_on_access(plru[index], 1);
+                accessed_line <= 1;
             end else if (!info2.valid && read_en_mem && write_en_cache) begin
                 cache[index][2][0] <= 1;
                 cache[index][2][1] <= 0;
                 cache[index][2][TAG_WIDTH+1:2] <= tag;
                 cache[index][2][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                update_tree_on_access(plru[index], 2);
+                accessed_line <= 2;
             end else if (!info3.valid && read_en_mem && write_en_cache) begin
                 cache[index][3][0] <= 1;
                 cache[index][3][1] <= 0;
                 cache[index][3][TAG_WIDTH+1:2] <= tag;
                 cache[index][3][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                update_tree_on_access(plru[index], 3);
+                accessed_line <= 3;
             end
             else if (read_en_mem && write_en_cache) begin // MISS WHEN CACHE IS NOT EMPTY 
-                case (lru)
+                case (lru_line)
                     0: if (!info0.dirty) begin //DIRTY BIT IS ZERO
                         cache[index][0][0] <= 1;
                         cache[index][0][1] <= 0;
                         cache[index][0][TAG_WIDTH+1:2] <= tag;
                         cache[index][0][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                        update_tree_on_access(plru[index], 0);
+                        accessed_line <= 0;
                     end else if (read_en_cache && write_en_mem) begin //dirtybit is 1
                         dirty_block_out <= info0.block;
                         cache[index][0][1] <= 0;
@@ -122,7 +132,7 @@ module cache_memory #(
                         cache[index][1][1] <= 0;
                         cache[index][1][TAG_WIDTH+1:2] <= tag;
                         cache[index][1][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                        update_tree_on_access(plru[index], 1);
+                        accessed_line <= 1;
                     end else if (read_en_cache && write_en_mem) begin
                         dirty_block_out <= info1.block;
                         cache[index][1][1] <= 0;
@@ -132,7 +142,7 @@ module cache_memory #(
                         cache[index][2][1] <= 0;
                         cache[index][2][TAG_WIDTH+1:2] <= tag;
                         cache[index][2][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                        update_tree_on_access(plru[index], 2);
+                        accessed_line <= 2;
                     end else if (read_en_cache && write_en_mem) begin
                         dirty_block_out <= info2.block;
                         cache[index][2][1] <= 0;
@@ -142,7 +152,7 @@ module cache_memory #(
                         cache[index][3][1] <= 0;
                         cache[index][3][TAG_WIDTH+1:2] <= tag;
                         cache[index][3][BLOCK_SIZE + TAG_WIDTH + 1 : TAG_WIDTH + 2] <= data_in_mem;
-                        update_tree_on_access(plru[index], 3);
+                        accessed_line <= 3;
                         end else if (read_en_cache && write_en_mem) begin
                         dirty_block_out <= info3.block;
                         cache[index][3][1] <= 0;
@@ -154,35 +164,41 @@ module cache_memory #(
                 if (info0.hit) begin
                     cache[index][0][TAG_WIDTH + 2 + blk_offset * WORD_SIZE +: WORD_SIZE] <= data_in;
                     cache[index][0][1] <= 1;
-                    update_tree_on_access(plru[index], 0);
+                   accessed_line <= 0;
                 end else if (info1.hit) begin
                     cache[index][1][TAG_WIDTH + 2 + blk_offset * WORD_SIZE +: WORD_SIZE] <= data_in;
                     cache[index][1][1] <= 1;
-                    update_tree_on_access(plru[index], 1);
+                    accessed_line <= 1;
                 end else if (info2.hit) begin
                     cache[index][2][TAG_WIDTH + 2 + blk_offset * WORD_SIZE +: WORD_SIZE] <= data_in;
                     cache[index][2][1] <= 1;
-                    update_tree_on_access(plru[index], 2);
+                    accessed_line <= 2;
                 end else if (info3.hit) begin
                     cache[index][3][TAG_WIDTH + 2 + blk_offset * WORD_SIZE +: WORD_SIZE] <= data_in;
                     cache[index][3][1] <= 1;
-                    update_tree_on_access(plru[index], 3);
+                     accessed_line <= 3;
                 end
             end else if (!req_type && read_en_cache) begin // Read on hit
                 if (info0.hit) begin
                     data_out <= info0.block[blk_offset*WORD_SIZE +: WORD_SIZE];
-                    update_tree_on_access(plru[index], 0);
+                     accessed_line <= 0;
                 end else if (info1.hit) begin
                     data_out <= info1.block[blk_offset*WORD_SIZE +: WORD_SIZE];
-                    update_tree_on_access(plru[index], 1);
+                    accessed_line <= 1;
                 end else if (info2.hit) begin
                     data_out <= info2.block[blk_offset*WORD_SIZE +: WORD_SIZE];
-                    update_tree_on_access(plru[index], 2);
+                    accessed_line <= 2;
                 end else if (info3.hit) begin
                     data_out <= info3.block[blk_offset*WORD_SIZE +: WORD_SIZE];
-                    update_tree_on_access(plru[index], 3);
+                    accessed_line <= 3;
                 end
             end
         end
+        if (accessed_line !== 'x) begin
+            plru[index].b1 <= plru_next[2];
+            plru[index].b2 <= plru_next[1];
+            plru[index].b3 <= plru_next[0];
+        end
     end
+    
 endmodule
