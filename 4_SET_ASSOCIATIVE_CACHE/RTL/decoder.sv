ac82f66ac47607d768c1c86a4da9aedfe5cd02ea
Ammarahwakeel <ammarahwakeel9@gmail.com>
2025-10-08 03:06:18 -0700

    Update decoder.sv

--- 4_SET_ASSOCIATIVE_CACHE/RTL/decoder.sv
+++ 4_SET_ASSOCIATIVE_CACHE/RTL/decoder.sv
@@ -1,6 +1,13 @@
+// Copyright 2025 Maktab-e-Digital Systems Lahore.
+// Licensed under the Apache License, Version 2.0, see LICENSE file for details.
+// SPDX-License-Identifier: Apache-2.0
+// Author:  Ammarah Wakeel, Ayesha Anwar, Eman Nasarrr.
+// Date: 5th, August, 2025.
+// CACHE CONTROLLER IS SAME AS DIRECT MAPPED CACHE, 2 WAY CACHE AND N WAY CACHE
+
 //==============================================================
 //  Module: cache_decoder
-//  Function: Decodes a 32-bit memory address into tag, index,
+//  Description: Decodes a 32-bit memory address into tag, index,
 //            and block offset fields for a cache.
 //==============================================================
 
