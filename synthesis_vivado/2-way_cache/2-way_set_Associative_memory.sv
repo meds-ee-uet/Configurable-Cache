6563f641a59a56b053d8dd5e918c0f0e3508f670
Ammarahwakeel <ammarahwakeel9@gmail.com>
2025-10-08 03:43:19 -0700

    Update 2-way_set_Associative_memory.sv

--- synthesis_vivado/2-way_cache/2-way_set_Associative_memory.sv
+++ synthesis_vivado/2-way_cache/2-way_set_Associative_memory.sv
@@ -1,3 +1,11 @@
+// Copyright 2025 Maktab-e-Digital Systems Lahore.
+// Licensed under the Apache License, Version 2.0, see LICENSE file for details.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Description: This file contains the RTL code 2 way set associative cache memory module to synthesize and implement it in vivado
+//
+// Author: Ammarah Wakeel.
+// Date: 20th, August, 2025.
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
